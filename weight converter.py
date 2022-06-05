7ff7c98c333579b06b2551063e06ff65c2e167cd
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-05 20:47:49 +0200

    Update weight converter.py

--- weight converter.py
+++ weight converter.py
@@ -1,13 +1,11 @@
-weight = int(input('Hello. I am a weight converter. Can you tell me how much you weigh? '))
-unit = input("Thank you. Now, can you tell me , is that in (Lbs) or (Kilos)? ")
+weight = int(input("Hi there, I am a weight converter. How much do you weigh? "))
+unit = input("Thanks. Can you tell me if that is in (Lbs) or (Kilos)? ")
 if unit.upper() == "L":
     converted = weight * 0.45
-    print(f'''
-Great. In kilos, you weigh {converted}''')
+    print(f"Okay, thanks. Your wieght in kilos is {converted}")
 else:
     converted = weight // 0.45
-    print(f'''
-Great. In lbs, you weight {converted}''')
+    print(f"Okay, thanks. Your weight in lbs is {converted}")
 
 print('''
-Now lose weight, fat ass.''')
+Now go get on a treadmill and stop eating Doritos!''')

cf1101ab4032474567f8bcfcdb572e7ea77f16b9
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-05 19:04:19 +0200

    Create Logical Operators Code High Income AND Good Credit.py

---
+++ Logical Operators Code High Income AND Good Credit.py
@@ -0,0 +1,10 @@
+ #Logical Operators. If one statement AND another statement are both true, then statement follows.
+ #Example: If borrower has high income AND good credit, then they are eligible for a loan.
+ #Note: Cal also use "or" operator, which means only one of the conditions needs to be true.
+
+has_high_income = True
+has_good_credit = True
+
+if has_good_credit and has_high_income:
+     print("YOu are eligible for a loan you lucky SOB")
+else: print("You sorry son of a bitch. Get out!")

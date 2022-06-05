b3d69967686eb1c54afbafc705659ad0048a1aff
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-05 19:09:53 +0200

    Update Logical Operators Code High Income AND Good Credit.py

--- Logical Operators Code High Income AND Good Credit.py
+++ Logical Operators Code High Income AND Good Credit.py
@@ -6,5 +6,5 @@ has_high_income = True
 has_good_credit = True
 
 if has_good_credit and has_high_income:
-     print("YOu are eligible for a loan you lucky SOB")
+     print("You are eligible for a loan you lucky SOB")
 else: print("You sorry son of a bitch. Get out!")

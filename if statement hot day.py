e55591a358ab0ef9f24ff19daff63676ef9f74a7
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-05 18:07:27 +0200

    Python with Mosh YouTube Course
    I'm learning Python and this course seems to make the most sense to me. It's the beginning of my Python journey. Everything is synced from PyCharm and I plan to complete this 6 hour course. Yay!

--- if statement hot day.py
+++ if statement hot day.py
@@ -1,2 +1,11 @@
-is_hot = True
 is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's gettin' hot in here, so take off all your clothes")
+    print("I am getting so hot, I'm gonna take my clothes off")
+elif is_cold:
+    print("Whoa, it's chilly up in this piece. Definitely put all your clothes back on")
+else:
+    print("It's just a pleasant day. Enjoy  your day! Please come again")
+

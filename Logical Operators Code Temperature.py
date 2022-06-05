8a302c00023dd330295625ccf86349fd09ef49b0
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-05 19:32:04 +0200

    Create Logical Operators Code Temperature.py

---
+++ Logical Operators Code Temperature.py
@@ -0,0 +1,8 @@
+#Boolean expression (if temperature > 30:) because it contains a value.
+
+temperature = 30
+
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")

65eff15783f356ddd50abf8a160c9141c5f125a8
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-06 21:44:47 +0200

    Update scratch file.py

--- scratch file.py
+++ scratch file.py
@@ -1,13 +1,14 @@
 
-secret_number = 9
+secret_number = 98
 guess_count = 0
 guess_limit = 3
 
 while guess_count < guess_limit:
-    guess = int(input("Guess: "))
+    guess = int(input("What is your guess? "))
     guess_count += 1
     if guess == secret_number:
-        print("You win!")
+        print("Wow. You got it. You win!")
         break
 else:
-    print("You lose")
+    print("Shit, sorry. You lose. You a loser")
+

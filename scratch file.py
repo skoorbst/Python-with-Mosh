a6f2df5b354afa0a8fa9bfe462dc396017229e7e
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-09 14:55:12 +0200

    Update scratch file.py

--- scratch file.py
+++ scratch file.py
@@ -1,14 +1,10 @@
+prices = [10982, 3978, 2]
+total = 0
 
-is_hot = False
-is_cold = False
+for price in prices:
+    total += price
+print(f"Total = {total}")
 
-if is_hot:
-    print("It is hot. Drink water")
-elif is_cold:
-    print("It is cold. Wear a sweater")
-else:
-    print("It's a nice day")
-print("Have a great day")
 
 
 

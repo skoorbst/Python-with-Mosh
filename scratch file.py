77b56bf17d783c9715364b9f19b603ce96f3370d
skoorbst <71600867+skoorbst@users.noreply.github.com>
2022-06-09 20:54:52 +0200

    Update scratch file.py

--- scratch file.py
+++ scratch file.py
@@ -1,15 +1,21 @@
-numbers = [20, 3, 276, 1990, 208, 6]
+#find biggest number in a list
+
+numbers = [3, 2, 4, 5, 6087267, 122873894]
 max = 0
 for number in numbers:
     if number > max:
         max = number
 print(max)
 
-prices = [20, 3, 276, 1990, 208, 6]
+#total value of items in a cart
+
+prices = [3, 2, 4, 5, 6087267, 122873894]
 total = 0
 for price in prices:
     total += price
-print(f"Price {total}")
+print(f"Total {total}")
+
+
 
 
 
